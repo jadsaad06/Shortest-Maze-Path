4126a59f2489ab82b081c367978817352825f1b4
Jad Saad <jadsaad06@gmail.com>
2024-11-18 21:51:07 -0800

    Added a maze generator to generate 5x5 mazes for testing

--- maze.cpp
+++ maze.cpp
@@ -8,44 +8,33 @@ using namespace std;
 
 void BFS(const vector<vector<int>>& mazeAdj, vector<bool>& visited, vector<int>& parent, vector<int>& availableNodes);
 vector<int> findPath(vector<int>& parent, int end, int start);
+vector<vector<int>> genMaze();
 
 int main(){
-
-    //                               1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
-    vector<vector<int>> mazeAdj = { {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
-                                    {0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
-                                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}
-                            };
+    srand(time(0));
+    int counter = 0;
     
-    size_t size = mazeAdj.size();
+    for(int i = 0; i < 1000; i++){
+        vector<vector<int>> mazeAdj = genMaze();    
+        size_t size = mazeAdj.size();
 
-    vector<bool> visited(size, false); //set size of visited to amnt nodes, set all unvisited
-    vector<int> parent(size, -1);
-    vector<int> availableNodes(size);
+        vector<bool> visited(size, false); //set size of visited to amnt nodes, set all unvisited
+        vector<int> parent(size, -1);
+        vector<int> availableNodes(size);
 
-    int start = sqrt(size) - 1;
-    int end = size - start;
+        int start = sqrt(size) - 1;
+        int end = size - start;
 
-    availableNodes.push_back(start);
-    BFS(mazeAdj, visited, parent, availableNodes);
-    vector<int> path = findPath(parent, end, start);
-    
-    for(int i : path){
-        cout << " " << i + 1;
+        availableNodes.push_back(start);
+        BFS(mazeAdj, visited, parent, availableNodes);
+        vector<int> path = findPath(parent, end, start);
+
+        if(path[0] != -1){
+            counter++;
+        }
     }
+        
+    cout << counter;
     
     return 0;
 }
@@ -84,4 +73,28 @@ vector<int> findPath(vector<int>& parent, int end, int start){
 
 
     return {-1};
+}
+
+vector<vector<int>> genMaze() {
+    vector<vector<int>> maze(25, vector<int>(25, 0));
+
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 25; j++) {
+            if (i != j) { // Skip self-loop
+
+                if (i / 5 == j / 5 && abs(i - j) == 1) { //horizontal adjacent
+                    int random = rand() % 2;
+                    maze[i][j] = random;
+                    maze[j][i] = random;
+                }
+                else if (abs(i - j) == 5) { //vertical adjacent
+                    int random = rand() % 2;
+                    maze[i][j] = random;
+                    maze[j][i] = random;
+                }
+            }
+        }
+    }
+
+    return maze;
 }
